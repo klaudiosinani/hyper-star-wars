82d875dfd644d320cb8db7b5d8da923a6357ad6b
Klaus Sinani <klauscfhq@protonmail.com>
2017-07-24 21:31:21 +0300

    :fish_cake:Minor update

--- index.js
+++ index.js
@@ -2,39 +2,68 @@ const fs = require('fs');
 const yaml = require('js-yaml');
 const homeDir = require('home-dir');
 
-const path = homeDir('/.hyper_plugins/local/hyper-star-wars/backgrounds/');
+const path = homeDir('/.hyper_plugins/node_modules/hyper-star-wars/backgrounds/');
 const extension = '.png';
 
 exports.decorateConfig = config => {
 	let theme;
 	let keys;
 	let index;
-	const getTheme = config.theme;
+	const getTheme = Array.isArray(config.character) ? config.character[Math.floor(Math.random() * config.character.length)] : config.character;
 	let swTheme = getTheme.toLowerCase();
+	const unibody = config.unibody;
+	const unibodyFlag = unibody !== 'false';
+
+	// Load color palettes from yaml files
+	const colorSchemesYml = yaml.safeLoad(
+		fs.readFileSync(
+			homeDir('/.hyper_plugins/node_modules/hyper-star-wars/color-schemes.yml'),
+			'utf8'
+		)
+	);
+
+	// Determine theme color palette
+	if (swTheme === 'random') {
+		keys = Object.keys(colorSchemesYml.character);
+		index = Math.floor(Math.random() * (keys.length));
+		swTheme = keys[index];
+	}
+
+	if (Object.prototype.hasOwnProperty.call(colorSchemesYml.character, swTheme)) {
+		theme = colorSchemesYml.character[swTheme];
+	} else {
+		theme = colorSchemesYml.default;
+	}
+
+	// Set theme colors
+	const primary = (unibodyFlag === true) ? theme.unibody : theme.header;
+	const secondary = '#383A42';
+	const tertiary = '#383A42';
+	const selectedColor = theme.primary;
 
 	const syntax = {
 		dark: {
-			borderColor: '#383A42',
-			cursorColor: '#383A42',
-			foregroundColor: '#383A42',
+			borderColor: primary,
+			cursorColor: secondary,
+			foregroundColor: secondary,
 			backgroundColor: '#383A42',
 			colors: {
-				black: '#383A42',
-				red: '#383A42',
-				green: '#383A42',
-				yellow: '#383A42',
-				blue: '#383A42',
-				magenta: '#383A42',
-				cyan: '#383A42',
-				white: '#383A42',
-				lightBlack: '#383A42',
-				lightRed: '#383A42',
-				lightGreen: '#383A42',
-				lightYellow: '#383A42',
-				lightBlue: '#383A42',
-				lightMagenta: '#383A42',
-				lightCyan: '#383A42',
-				lightWhite: '#383A42'
+				black: tertiary,
+				red: secondary,
+				green: tertiary,
+				yellow: secondary,
+				blue: secondary,
+				magenta: secondary,
+				cyan: secondary,
+				white: secondary,
+				lightBlack: tertiary,
+				lightRed: secondary,
+				lightGreen: secondary,
+				lightYellow: secondary,
+				lightBlue: secondary,
+				lightMagenta: secondary,
+				lightCyan: secondary,
+				lightWhite: secondary
 			}
 		}
 	};
@@ -48,10 +77,17 @@ exports.decorateConfig = config => {
 		pathToTheme = assemblePath;
 	}
 
-
-	return Object.assign({}, config, syntax.dark, {
+	return Object.assign({}, config,
+		syntax.dark, {
 			termCSS: `
           ${config.termCSS || ''}
+          ::selection {
+            background: ${selectedColor} !important;
+          }
+          ::-webkit-scrollbar-thumb {
+            background-color: ${secondary};
+            -webkit-box-shadow: none;
+					}
           x-screen {
             background: transparent !important;
           }
@@ -63,33 +99,33 @@ exports.decorateConfig = config => {
           background-size: cover;
         }
         .header_header, .header_windowHeader {
-          background-color: '#383A42' !important;
+          background-color: ${primary} !important;
         }
         .tabs_nav .tabs_list {
           border-bottom: 0;
         }
         .tabs_nav .tabs_title,
         .tabs_nav .tabs_list .tab_tab {
-          color: '#383A42';
+          color: ${secondary};
           border: 0;
         }
         .tab_icon {
-          color: '#383A42';
+          color: ${secondary};
         }
         .tab_icon:hover {
-          background-color: '#383A42';
+          background-color: ${secondary};
         }
         .tabs_nav .tabs_list .tab_tab:not(.tab_active) {
           background-color: rgba(0,0,0,0.1);
         }
         .tabs_nav .tabs_list {
-          color: '#383A42';
+          color: ${primary};
         }
         .tabs_nav .tabs_list .tab_active .tab_text {
-          border-bottom: 3px solid '#383A42';
+          border-bottom: 3px solid ${secondary};
         }
         .terms_terms .terms_termGroup .splitpane_panes .splitpane_divider {
-          background-color: '#383A42' !important;
+          background-color: ${secondary} !important;
         }
       `
 		}
